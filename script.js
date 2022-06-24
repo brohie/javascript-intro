f6fe2d6f3691692685b262bb8263eaa76f382ea2
brohie <105175456+brohie@users.noreply.github.com>
2022-06-24 05:17:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+const name = document.getElementById('name').innerText = 'Oghenebrohie Nyerohvwo';
+const height = document.getElementById('height').innerText = '6 Foot';
+const country = document.getElementById('country').innerText = 'Nigeria';
